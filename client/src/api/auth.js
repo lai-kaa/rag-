2d0fd1d7648dd7efb9be850e1dbc7cbcb695dd87
lai-kaa <3579531196@qq.com>
2026-07-04 23:09:08 +0800

    更换接口拼接方式，彻底解决404

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,12 +1,6 @@
-/**
- * 认证相关 API
- */
-import request from './request'
-
 export function login(data) {
-  return request.post('/auth/login', data)
+  return request.post('/api/auth/login', data)
 }
-
 export function getMe() {
-  return request.get('/auth/me')
+  return request.get('/api/auth/me')
 }
